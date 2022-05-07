4e831c7bb6e5d98d34f46309b75cb369e2b9cb2b
Jake Son <sonjeabin@gmail.com>
2022-05-07 23:41:25 +0900

    feature: graphql DateTime 타입추가

--- app/src/main/kotlin/com/todo/app/TodoAppApplication.kt
+++ app/src/main/kotlin/com/todo/app/TodoAppApplication.kt
@@ -1,16 +1,24 @@
 package com.todo.app
 
+import com.expediagroup.graphql.generator.directives.KotlinDirectiveWiringFactory
+import com.todo.app.hook.CustomSchemaGeneratorHooks
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
 @EntityScan(basePackages = ["com.todo.lib.entity"])
 @SpringBootApplication
-class AppApplication
+class AppApplication {
+  @Bean fun wiringFactory() = KotlinDirectiveWiringFactory()
+
+  @Bean
+  fun hooks(wiringFactory: KotlinDirectiveWiringFactory) = CustomSchemaGeneratorHooks(wiringFactory)
+}
 
 fun main(args: Array<String>) {
   runApplication<AppApplication>(*args)
