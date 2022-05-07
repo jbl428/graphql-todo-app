b5d50593b611d7673d582f69dac46a1fc0c5b836
Jake Son <sonjeabin@gmail.com>
2022-05-07 18:03:23 +0900

    feature: 기본 엔티티 추가

---
+++ lib/src/main/kotlin/com/todo/lib/entity/BaseEntity.kt
@@ -0,0 +1,22 @@
+package com.todo.lib.entity
+
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long = 0
+
+  @CreatedDate @Column(nullable = false, updatable = false) lateinit var createdAt: LocalDateTime
+
+  @LastModifiedDate @Column(nullable = false) lateinit var updatedAt: LocalDateTime
+}
