99c626fd073903bdc631fd94d6d73bd2274481bb
Jake Son <sonjeabin@gmail.com>
2022-05-08 00:57:40 +0900

    test: graphql 스키마 생성 테스트 구현

--- app/src/test/kotlin/com/todo/app/TodoApplicationTests.kt
+++ app/src/test/kotlin/com/todo/app/TodoApplicationTests.kt
@@ -1,10 +1,33 @@
 package com.todo.app
 
+import io.kotest.matchers.nulls.shouldNotBeNull
+import java.io.File
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
-internal class TodoApplicationTests {
+@AutoConfigureWebTestClient
+internal class TodoApplicationTests(
+  @Autowired private val webTestClient: WebTestClient,
+) {
+  @Test
+  fun generateSchema() {
+    // when
+    val schema =
+      webTestClient
+        .get()
+        .uri("/sdl")
+        .exchange()
+        .returnResult(String::class.java)
+        .responseBody
+        .reduce { a, b -> a + "\n" + b }
+        .block()
 
-  @Test fun contextLoads() {}
+    // then
+    schema.shouldNotBeNull()
+    File("src/main/resources/graphql/schema.graphql").writeText(schema)
+  }
 }
