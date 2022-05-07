d9c18e535bfdd0af77aa2a6eea8382322aed6076
Jake Son <sonjeabin@gmail.com>
2022-05-07 18:04:20 +0900

    feature: task 엔티티 추가

---
+++ lib/src/main/kotlin/com/todo/lib/entity/task/Task.kt
@@ -0,0 +1,18 @@
+package com.todo.lib.entity.task
+
+import com.todo.lib.entity.BaseEntity
+import com.todo.lib.entity.user.User
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
+import javax.validation.constraints.NotNull
+
+@Entity
+class Task(
+  @Column(nullable = false) var name: String,
+  @Column(nullable = false) var completed: Boolean,
+  @Column var completedAt: LocalDateTime?,
+  @ManyToOne(fetch = FetchType.LAZY) @field:NotNull var user: User,
+) : BaseEntity()
