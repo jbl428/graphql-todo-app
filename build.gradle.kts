d4cb9be43c62d5bd266b9bbc8e446655bd1421d5
Jake Son <sonjeabin@gmail.com>
2022-05-08 11:36:05 +0900

    chore: 미사용 패키지 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ subprojects {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("io.arrow-kt:arrow-core:1.0.1")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -39,7 +38,6 @@ subprojects {
     }
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("io.mockk:mockk:1.12.3")
-    testImplementation("io.kotest:kotest-assertions-core:5.2.3")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
