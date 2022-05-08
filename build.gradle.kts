ba65746cb64d97f40be68589e20f30c97004c88d
Jake Son <sonjeabin@gmail.com>
2022-05-08 11:35:32 +0900

    style: gradle 파일 포매팅 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,58 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.7" apply false
-	id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-	id("com.expediagroup.graphql") version "5.3.2" apply false
-	id("com.ncorti.ktfmt.gradle") version "0.8.0"
+  id("org.springframework.boot") version "2.6.7" apply false
+  id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+  id("com.expediagroup.graphql") version "5.3.2" apply false
+  id("com.ncorti.ktfmt.gradle") version "0.8.0"
 
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21" apply false
-	kotlin("plugin.jpa") version "1.6.21" apply false
+  kotlin("jvm") version "1.6.21"
+  kotlin("plugin.spring") version "1.6.21" apply false
+  kotlin("plugin.jpa") version "1.6.21" apply false
 }
 
 repositories { mavenCentral() }
 
 subprojects {
-	group = "com.todo"
-	version = "0.0.1-SNAPSHOT"
+  group = "com.todo"
+  version = "0.0.1-SNAPSHOT"
 
-	repositories { mavenCentral() }
+  repositories { mavenCentral() }
 
-	apply(plugin = "java")
-	apply(plugin = "kotlin")
-	apply(plugin = "com.ncorti.ktfmt.gradle")
+  apply(plugin = "java")
+  apply(plugin = "kotlin")
+  apply(plugin = "com.ncorti.ktfmt.gradle")
 
-	java.sourceCompatibility = JavaVersion.VERSION_11
+  java.sourceCompatibility = JavaVersion.VERSION_11
 
-	dependencies {
-		implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-		implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-		implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-		implementation("io.arrow-kt:arrow-core:1.0.1")
-		implementation("org.springframework.boot:spring-boot-starter-validation")
+  dependencies {
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.arrow-kt:arrow-core:1.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
-		testImplementation("org.springframework.boot:spring-boot-starter-test") {
-			exclude(module = "mockito-core")
-		}
-		testImplementation("io.projectreactor:reactor-test")
-		testImplementation("io.mockk:mockk:1.12.3")
-		testImplementation("io.kotest:kotest-assertions-core:5.2.3")
-		testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
-		testImplementation("com.ninja-squad:springmockk:3.1.1")
-		testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
-	}
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+      exclude(module = "mockito-core")
+    }
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.mockk:mockk:1.12.3")
+    testImplementation("io.kotest:kotest-assertions-core:5.2.3")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+  }
 
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs = listOf("-Xjsr305=strict")
-			jvmTarget = "11"
-		}
-	}
+  tasks.withType<KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = "11"
+    }
+  }
 
-	ktfmt { googleStyle() }
+  ktfmt { googleStyle() }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+  tasks.withType<Test> { useJUnitPlatform() }
 }
-
